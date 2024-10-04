483976b5d3ebdbbe0bb17b2be791bec8f7337341
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-04 03:29:36 +0300

    conventional middleware

--- WebApplication/Middleware/Program.cs
+++ WebApplication/Middleware/Program.cs
@@ -1,26 +1,32 @@
-﻿using WebApplication1;
+﻿using Middleware;
+using Middleware.Extencation;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<CustomeMiddleware>();
+builder.Services.AddTransient<CustomeMiddleware>(); //added as extencations
 var app = builder.Build();
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-await context.Response.WriteAsync("Atheer ....");
-     await next(context);// in case we make as comment this line the result will be Atheer ....
-});
 
-app.UseMiddleware<CustomeMiddleware>();
 
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync(" Alenazi....");
+    await context.Response.WriteAsync("Atheer ...."); 
+    await next(context);// in case we make as comment this line the result will be Atheer ....
+});//added as lamda excpration 
+
+
+app.UseMiddleware<CustomeMiddleware>();//added as middleware class 
+
+app.Use(async (HttpContext context, RequestDelegate next) => 
+{
+    await context.Response.WriteAsync("Alenazi....");
     await next(context);
-});
+});//added as lamda excpration 
 
 
-app.CallMiddleware();
+app.CallMiddleware();//added as extencations
 
+app.UseConventionalMiddleware(); //added as conventional middleware class
 app.Run();
 
-// 
+//the result will be
+//Atheer .... @ great @ Alenazi.... @ great @
 
