5fa3f6938a4ab60635c0d76ff181c92d5e30e300
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-04 03:00:17 +0300

    add Middleware project

---
+++ WebApplication/Middleware/CustomeMiddleware.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WebApplication1
+{
+    public class CustomeMiddleware : IMiddleware
+    {
+      public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+          await  context.Response.WriteAsync( " @ grate @");
+            await next(context);
+           
+        }
+    }
+
+    public static class Custome
+    {
+
+        public static IApplicationBuilder CallMiddleware(this IApplicationBuilder app)
+        {
+           return app.UseMiddleware<CustomeMiddleware>();
+        }
+    }
+}
---
+++ WebApplication/Middleware/Program.cs
@@ -0,0 +1,26 @@
+﻿using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<CustomeMiddleware>();
+var app = builder.Build();
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+await context.Response.WriteAsync("Atheer ....");
+     await next(context);// in case we make as comment this line the result will be Atheer ....
+});
+
+app.UseMiddleware<CustomeMiddleware>();
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync(" Alenazi....");
+    await next(context);
+});
+
+
+app.CallMiddleware();
+
+app.Run();
+
+// 
+
