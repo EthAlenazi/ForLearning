db3f07e7ad0375d0890966c01ce72fcc574781be
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-12 22:01:16 +0300

    first comment

--- WebApplication/Routing/Program.cs
+++ WebApplication/Routing/Program.cs
@@ -1,6 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Use(async (context, next) => {
+    Endpoint? endpoint = context.GetEndpoint();// return details for the request if route is found, allwas will be null 
+    if (endpoint != null)
+        await context.Response.WriteAsync($"{endpoint.DisplayName} , {endpoint.RequestDelegate.ToString()},{endpoint.Metadata}");
+    await next();
+});
+app.UseRouting(); // for find the route 
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) => {
+    Endpoint? endpoint = context.GetEndpoint();// return details for the request if route is found
+    if (endpoint != null)
+        await context.Response.WriteAsync($"{endpoint.DisplayName} , {endpoint.RequestDelegate},{endpoint.Metadata}");
+    await next();
+});
+app.UseEndpoints(endpoints => // for the excute the route
+endpoints.MapGet("api/product/{id}",async
+   ( context)=>{
+       await context.Response.WriteAsync("\n your are in api/product/{id}");
+    }));
 
+app.UseEndpoints(endpoints => // for the excute the route
+endpoints.MapGet("api/product/{id?}", async
+   (context) => {
+       await context.Response.WriteAsync("\n your are in api/product/{id?}!!");
+   }));
+app.UseEndpoints(endpoints => // for the excute the route
+endpoints.MapGet("api/product/{id:int}", async
+   (context) => {
+       await context.Response.WriteAsync("\n your are in api/product/{id:int}!!");
+   }));
+app.UseEndpoints(endpoints =>
+endpoints.MapGet("api/data/{note:alpha:length(1,5)}", async
+   (context) => {
+       await context.Response.WriteAsync(" your are in alpha:length!!");//Route Constraints
+   }));
+app.UseStaticFiles();//mvc project
+app.Run(context=>context.Response.WriteAsync("This url not correct!"));//for the default route
 app.Run();
