69bd56fe8a277291af7be471029ad994a606e270
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-14 20:50:55 +0300

    add controller as class

---
+++ WebApplication/ControllersConcept/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersConcept.Controllers
+{
+    public class HomeController
+    {
+        [Route("Data")]
+        public string Data()
+        {
+            return "Hello from HomeController.Data()!";
+        }
+    }
+}
