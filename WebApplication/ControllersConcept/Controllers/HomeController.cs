47111cf159a5d7dd83d1640a27be8b54c9976038
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-19 11:57:07 +0300

    return type ContentResult

--- WebApplication/ControllersConcept/Controllers/HomeController.cs
+++ WebApplication/ControllersConcept/Controllers/HomeController.cs
@@ -3,15 +3,26 @@
 namespace ControllersConcept.Controllers
 {
     [Controller]// if we don't add Controller in class name we should add attribute
-    public class HomeController   {
+    public class HomeController :Controller
+    {
+        [Route("index")]
+        public ContentResult Index()
+        {
+            //return new ContentResult() { Content = "oky", 
+            //    ContentType = "Atheer" //can be anything 
+            //};// old way without using inherited class controller  
+           
+            //return Content("oky","Atheer");  
+            return Content("oky");
+        }
         [Route("Data")]
         [Route("/")]
         public string Data()
         {
             return "Hello from HomeController.Data()!";
         }
-        [Route("contact/{moblile:regex(^\\d{{10}}$)}")]// (/d) just accept digets 
-        public string Data(int moblile)//the rout shoul be contact/1234567890
+        [Route("contact/{moblile:regex(^\\d{{10}}$)}")]// (/d) just accept digits 
+        public string Data(int moblile)//the rout should be contact/1234567890
         {
             return "Hello from HomeController.Data()!";
         }
