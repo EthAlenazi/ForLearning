28349e61ee6ce54285ec75e49f2e7237abc71aab
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-19 11:27:04 +0300

    added multiple route and declare how can implement the controller with Controller key word

--- WebApplication/ControllersConcept/Controllers/HomeController.cs
+++ WebApplication/ControllersConcept/Controllers/HomeController.cs
@@ -2,10 +2,16 @@
 
 namespace ControllersConcept.Controllers
 {
-    public class HomeController
-    {
+    [Controller]// if we don't add Controller in class name we should add attribute
+    public class HomeController   {
         [Route("Data")]
+        [Route("/")]
         public string Data()
+        {
+            return "Hello from HomeController.Data()!";
+        }
+        [Route("contact/{moblile:regex(^\\d{{10}}$)}")]// (/d) just accept digets 
+        public string Data(int moblile)//the rout shoul be contact/1234567890
         {
             return "Hello from HomeController.Data()!";
         }
