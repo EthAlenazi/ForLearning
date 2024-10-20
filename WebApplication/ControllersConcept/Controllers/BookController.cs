6b7935237c12a5a2b2a40b54dba1453d10f733ff
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-20 20:43:15 +0300

    return type IActionResult

---
+++ WebApplication/ControllersConcept/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersConcept.Controllers
+{
+    public class BookController : Controller
+    {
+        [Route("book")]
+        public IActionResult index()
+        {
+            //book id can't be null// empty
+            if (!Request.Query.ContainsKey("bookId"))
+            {
+                Response.StatusCode = 400;
+                return BadRequest("book Id is not applied!");
+
+            }
+            //book id should be between 1 to 1000
+            int bookId = Convert.ToInt32(ControllerContext.
+                HttpContext.Request.Query["bookId"]);
+            if (bookId <= 0)
+            {
+                Response.StatusCode = 400;
+                return BadRequest("book Id can't be lees then 0");
+            }
+
+            if (bookId > 1000)
+            {
+                Response.StatusCode = 400;
+                return BadRequest("book Id can't br greater then 1000");
+            }
+            //isLoggedin should be true 
+            if (Convert.ToBoolean(Request.Query["isLoggedin"]) == false)
+            {
+                Response.StatusCode = 401;
+                return BadRequest("User must be authenticated");
+            }
+            
+            return File("/AtheerALenazi.pdf", "application/pdf");
+            //if the type not apply correct the browser will not open file  
+
+        }
+    }
+}
