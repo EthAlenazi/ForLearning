950e77cda60d90aad54b798e6d4dc73cf9441796
Atheer <Alenazi.Atheer120@gmail.com>
2024-10-21 00:01:33 +0300

    return type LocalRedirectResult

--- WebApplication/ControllersConcept/Controllers/StoreController.cs
+++ WebApplication/ControllersConcept/Controllers/StoreController.cs
@@ -7,13 +7,23 @@ namespace ControllersConcept.Controllers
         [Route("store")]
         public IActionResult Index()
         {
-            ////RedirectToActionResult
+            ////RedirectToActionResult: in real world this is better 
+            ///based on controller name and action name 
 
             //return new RedirectToActionResult("GetResult", "Store", new {});//302 Found 
             //return new RedirectToActionResult("GetResult", "Store", new {},permanent :true);//301 Moved Permanently
             //return RedirectToAction("GetResult", "Store", new { });//shortcut 302 Found 
-            return RedirectToActionPermanent("GetResult", "Store", new { });//shortcut 301 Moved Permanently
-        }
+            //return RedirectToActionPermanent("GetResult", "Store", new { });//shortcut 301 Moved Permanently
+
+            ////LocalRedirectResult:  use direct url for local api
+            ///based on local specified url
+
+            //return new LocalRedirectResult("/store/book"); //302 Found
+            //return new LocalRedirectResult("/store/book",true);//shortcut 301 Moved Permanently 
+            //return LocalRedirect("/store/book");//302 Found
+            return LocalRedirectPermanent("/store/book");
+
+                   }
 
         [Route("store/book")]
         public IActionResult GetResult()
