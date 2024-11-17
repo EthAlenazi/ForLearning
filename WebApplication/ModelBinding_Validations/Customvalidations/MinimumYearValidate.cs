f3f4fddfb92d573f068fcaf0cdb803abc804cedd
Atheer <Alenazi.Atheer120@gmail.com>
2024-11-18 00:12:24 +0300

    add Customvalidations

---
+++ WebApplication/ModelBinding_Validations/Customvalidations/MinimumYearValidate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBinding_Validations.Customvalidations
+{
+    public class MinimumYearValidater:ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefaultErrorMessge = "Not alwoed to be less than {0}";
+        public MinimumYearValidater()
+        {
+            
+        }
+        public MinimumYearValidater(int minimumYear)
+        {
+            MinimumYear = minimumYear;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Year >= MinimumYear) {
+                    return new ValidationResult(string.Format( ErrorMessage?? DefaultErrorMessge,MinimumYear));
+
+                }
+
+                return ValidationResult.Success;
+            }
+
+            return null;
+        }
+    }
+}
--- WebApplication/ModelBinding_Validations/Models/Person.cs
+++ WebApplication/ModelBinding_Validations/Models/Person.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using ModelBinding_Validations.Customvalidations;
 
 namespace ModelBinding_Validations.Models
 {
     public class Person
     {
         [Required]
+        [Display(Name="Name")]
         [RegularExpression("^[A-Za-z .]*$")]
         [StringLength(50,MinimumLength=3)]
-        [Display(Name="Name")]
+      
         public string? Name { get; set; }
         [Phone]
         [Required]
@@ -26,6 +28,10 @@ namespace ModelBinding_Validations.Models
         public string? ConfirmPassword { get; set; }
         [Range(0,999.99)]
         public int Price { get; set; }
+        //[MinimumYearValidater(ErrorMessage ="Not Alowed the minimum year is 2000")]
+        // [MinimumYearValidater(ErrorMessage ="Not Alowed the minimum year is {0}")]//"Not Alowed the minimum year is 2002"
+        [MinimumYearValidater(2002)]//"Not alwoed to be less than 2002" DefaultErrorMessge
+        public DateTime DateOfBirth { get; set; }
 
         public override string ToString()  
         {
