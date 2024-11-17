3918b0efd6625b6cc327644d54a6a93776084224
Atheer <Alenazi.Atheer120@gmail.com>
2024-11-17 15:21:51 +0300

    Add Model Binding

---
+++ WebApplication/ModelBinding_Validations/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBinding_Validations.Controllers
+{
+    public class PersonController : Controller
+    {
+        //ModelBinding sort
+        //query string -4
+        /// <summary>
+        /// [FromQuery] 
+        /// data after ? in url 
+        /// data?id=2
+        /// </summary>
+        //Route Data   -3
+        ///<summary>
+        ///[FromRoute]
+        ///data applayed in url 
+        ///data/6/female
+        ///</summary>
+        //Request body -2
+        //from fields  -1
+        ///<summary>
+        ///data submitted by html forms
+        ///</summary>
+        ///
+        [Route("User/{name?}")]
+        public IActionResult Index(string name)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication/ModelBinding_Validations/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBinding_Validations.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? phoneNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Url { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public int Price { get; set; }
+    }
+}
